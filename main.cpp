24f4a385ccd29569940a964647a2e7b734d4a657
LUIGIFERRARACCIO <91878510+LUIGIFERRARACCIO@users.noreply.github.com>
2021-11-13 00:35:09 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,15 @@
 using namespace std;
 
 int main() {
-   return 0;
-}
+   int n=0;
+   cin>>n;
+    cout<<"1"<<endl;
+    int a=0, b=1, c=0;
+    for (int i=0; i<=n; i++)
+     {c=a+b;
+        a=b;
+        b=c;
+        cout<<c<<endl;
+    }
+    return 0;
+   }
